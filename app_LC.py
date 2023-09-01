d07466673c8a2daf962276e1d1314f4203c01fd9
sam916060 <81113420+sam916060@users.noreply.github.com>
2023-09-02 00:53:54 +0100

    Add files via upload

---
+++ app_LC.py
@@ -0,0 +1,86 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import r2_score
+from sklearn.model_selection import train_test_split
+
+# Load the dataset from your GitHub repository
+github_url = 'https://github.com/sam916060/python-random-quote/blob/master/LC-11.11.csv'
+dataset = pd.read_csv(github_url)
+
+# Page setup
+st.set_page_config(
+    page_title='Longitudinal Cracking Predictor',
+    page_icon='📈',
+    layout='wide'
+)
+
+# App title and description
+st.title('Longitudinal Cracking Predictor')
+st.subheader('Predicting Longitudinal Cracking based on Features')
+
+# Sidebar
+st.sidebar.header('Input Features')
+feature_values = {}
+
+# Collect user inputs for feature values
+for column in dataset.columns[:-1]:  # Exclude the last column (target)
+    feature_values[column] = st.sidebar.number_input(f'Enter {column} value', min_value=0.0)
+
+# Load the trained model from your GitHub repository
+model_url = 'https://github.com/sam916060/python-random-quote/blob/master/trained_model%20(1).pkl'
+model = joblib.load(model_url)  # Load the trained model from GitHub
+
+# Function to make predictions
+def predict_longitudinal_cracking(features):
+    # Replace this with your model prediction logic
+    # Example: prediction = model.predict([list(features.values())])
+    prediction = np.random.randint(0, 100)  # Replace with your prediction logic
+    return prediction
+
+# Prediction
+if st.sidebar.button('Predict'):
+    prediction = predict_longitudinal_cracking(feature_values)
+    st.subheader('Prediction Result')
+    st.write(f'The predicted longitudinal cracking value is: {prediction:.2f}')
+
+# Model Evaluation (optional)
+st.sidebar.header('Model Evaluation (R-squared Score)')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=16)
+regressor = RandomForestRegressor()  # Replace with your model
+regressor.fit(X_train, y_train)
+y_pred = regressor.predict(X_test)
+r2 = r2_score(y_test, y_pred)
+st.sidebar.write(f'R-squared (R2) Score on Test Data: {r2:.2f}')
+
+# Display dataset (optional)
+st.sidebar.header('Dataset')
+st.sidebar.write(dataset)
+
+# Footer
+st.sidebar.markdown(
+    """
+    **Note:** This is a simple example app for longitudinal cracking prediction. Actual results may vary depending on the model and data used.
+    """
+)
+
+# Display dataset (optional)
+st.write('## Dataset')
+st.dataframe(dataset)
+
+# About section
+st.write('## About')
+st.markdown(
+    """
+    This web app is designed to predict longitudinal cracking based on input features. 
+    It uses a simple random prediction for demonstration purposes. 
+    Replace the prediction logic with your trained model for accurate predictions.
+
+    To use this app, enter values for the features in the sidebar and click the "Predict" button.
+
+    This app is for demonstration purposes only and does not reflect real-world predictions.
+    """
+)
